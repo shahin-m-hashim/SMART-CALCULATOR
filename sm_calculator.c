c7f4abf8efd0908b4a2eb1a778e6060ec7b13482
Shahin M Hashim <98640366+shahin-m-hashim@users.noreply.github.com>
2023-01-17 22:06:34 +0530

    updated continue statement
    added a while loop for a continue statement after each choice entered and proceed accordingly

--- sm_calculator.c
+++ sm_calculator.c
@@ -29,31 +29,49 @@ double modulus(void)
 int main() 
 {
     double result;
-    char operation;
-    int n;
+    char operation,choice;
+    int n,flag;
 
-    printf("Enter operation (+, -, *, /, %): ");
-    scanf(" %c", &operation);
-
-    switch(operation) 
+    do
     {
-        case '+':
-            //switch case for addition
-            break;
-        case '-':
-            //switch case for subtraction
-        case '*':
-            //switch case for multiplication
-        case '/':
-            //switch case for division
-            break;
-        case '%':
-            //switch case for modulus
-            break;
-        default:
-            printf("Error: Invalid operation\n");
-            return 0;
-    }
+        printf("Enter operation (+, -, *, /, %): ");
+        scanf(" %c", &operation);
+
+        switch(operation) 
+        {
+            case '+':
+                //switch case for addition
+                break;
+            case '-':
+                //switch case for subtraction
+            case '*':
+                //switch case for multiplication
+            case '/':
+                //switch case for division
+                break;
+            case '%':
+                //switch case for modulus
+                break;
+            default:
+                printf("Error: Invalid operation\n");
+                return 0;
+        }
+        if(flag) 
+        {
+            printf("Do you want to continue (Y/N) ?\nChoice:");
+            scanf(" %c", &choice);
+            while(choice != 'Y' && choice != 'y' && choice != 'N' && choice != 'n') 
+            {
+                printf("Error: Invalid input. Please enter either 'Y' or 'N'\nChoice:");
+                fflush(stdin);
+                scanf(" %c", &choice);
+            }
+        }
+        else
+        {
+            choice = 'N';
+        }
+    }while(choice == 'Y' || choice == 'y');
     printf("\n");
     return 0;
 }
