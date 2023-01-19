e8cc5b93c12de6e2c174dec0b99a0c958dc6896a
Shahin M Hashim <98640366+shahin-m-hashim@users.noreply.github.com>
2023-01-19 17:04:36 +0530

    updated and simplified each function
    Now each function can easily perform integer validation of input based on the integer_validation function thereby providing more flexibility, efficiency, and reuse of code.

--- sm_calculator.c
+++ sm_calculator.c
@@ -4,6 +4,8 @@
 #include<ctype.h>
 #include<string.h>
 
+// INTEGER VALIDATION
+
 int getValidInteger() 
 {
 
@@ -28,189 +30,149 @@ int getValidInteger()
     return validInteger;
 }
 
+// ADDITION 
+
 double add(int n) 
 {
     double result = 0, num;
-    int check;
     for(int i = 0; i < n; i++) 
     {
-        do 
-        {
-            printf("Enter number %d: ", i+1);
-            check = scanf("%lf", &num);
-            if(check != 1) 
-            {
-                printf("Error: Invalid input. Please enter a valid number\n");
-                fflush(stdin);
-            }
-        }while(check != 1);
+        printf("Enter number %d: ", i+1);
+        num = getValidInteger();
         result += num;
     }
     return result;
 }
 
-double subtract(int n) 
-{
-    int check;
-    double result, num;
-    printf("Enter number 1: ");
-    scanf("%lf", &result);
-    for(int i = 1; i < n; i++) 
-    {
-        do
-        {
-            printf("Enter number %d: ", i+1);
-            check = scanf("%lf", &num);
-            if(check != 1) 
-            {
-                printf("Error: Invalid input. Please enter a valid number\n");
-                return 0;
-            }
-        }while(check != 1);
-        result -= num;
-    }
-    return result;
-}
+// SUBTRACTION - NEEDS FIXING
+
+// double subtract(int n) 
+// {
+//     double result, num;
+//     printf("Enter number 1: ");
+//     num = getValidInteger();
+//     for(int i = 1; i < n; i++) {
+//         printf("Enter number %d: ", i+1);
+//         num = getValidInteger();
+//         result -= num;
+//     }
+//     return result;
+// }
+
+// MULTIPLICATION - NEEDS FIXING
 
 double multiply(int n) 
 {
-    int check;
-    double result = 1, num;
+    double result = 0, num;
     for(int i = 0; i < n; i++) 
     {
-        do
-        {
-            printf("Enter number %d: ", i+1);
-            check = scanf("%lf", &num);
-            if(check != 1) 
-            {
-                printf("Error: Invalid input. Please enter a valid number\n");
-                return 0;
-            }
-        }while(check != 1);
+        printf("Enter number %d: ", i+1);
+        num = getValidInteger();            
         result *= num;
     }
     return result;
 }
 
+// DIVISION
+
 double divide(void) 
 {
-    int check;
-    double num1, num2;
-    printf("Enter first number: ");
-    check = scanf("%lf", &num1);
-        if(check != 1) 
-        {
-            printf("Error: Invalid input. Please enter a valid number\n");
-            return 0;
-        }
-    printf("Enter second number: ");
-    check = scanf("%lf", &num2);
-        if(check != 1) 
-        {
-            printf("Error: Invalid input. Please enter a valid number\n");
-            return 0;
-        }
-    if(num2 == 0) 
+    double divisor,divident;
+
+    printf("Enter The Divident:");
+    divident=getValidInteger();
+    printf("Enter The Divisor:");   
+    divisor=getValidInteger();
+
+    if(divisor == 0) 
     {
         printf("Error: Cannot divide by zero\n");
         return 0;
     }
-    return num1/num2;
+    return divident/divisor;
 }
+
+// MODULUS 
 
 double modulus(void) 
 {
-    int check;
-    double num1, num2;
-    printf("Enter first number: ");
-    check = scanf("%lf", &num1);
-        if(check != 1) 
-        {
-            printf("Error: Invalid input. Please enter a valid number\n");
-            return 0;
-        }
-    printf("Enter second number: ");
-    check = scanf("%lf", &num2);
-        if(check != 1) 
-        {
-            printf("Error: Invalid input. Please enter a valid number\n");
-            return 0;
-        }
-    if(num2 == 0) {
+    double divisor,divident;
+
+    printf("Enter The Divident:");
+    divident=getValidInteger();
+    printf("Enter The Divisor:");   
+    divisor=getValidInteger();
+
+    if(divisor == 0) 
+    {
         printf("Error: Cannot divide by zero\n");
         return 0;
     }
-    return (int)num1 % (int)num2;
+    return (int)divident % (int)divisor;
 }
+
+// MAIN APPLICATION
 
 int main() 
 {
-    double result;
     char choice;
-    int operation,n,flag=1;
+    double result;
+    int operation,n,flag;
+    
     printf("Available Operations\n1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n5.Modulus\n6.Exit\n");
     do
     {
         flag=1;
-        printf("What operation would you like to perform ?\nOperation:");
-        scanf("%d", &operation);
+        printf("\nWhat operation would you like to perform ?\nOperation:");
+        operation=getValidInteger();
 
         switch(operation) 
         {
-            int check1;
             case 1:         
-                printf("Enter the number of numbers: ");
-                do{
-                    check1 = scanf(" %d", &n);
-                    if(check1 != 1) {
-                        printf("Error: Invalid input. Please enter a valid number:");
-                        fflush(stdin);
-                    }
-                }while(check1 != 1);
+                printf("\nEnter the number of numbers: ");
+                n=getValidInteger();
                 result = add(n);
+                if(flag) printf("Result: %lf\n", result);
                 if(result == 0) flag = 0;
                 break;
 
             case 2:
-                printf("Enter the number of numbers: ");
-                do{
-                    check1 = scanf(" %d", &n);
-                    if(check1 != 1) {
-                        printf("Error: Invalid input. Please enter a valid number:");
-                        fflush(stdin);
-                    }
-                }while(check1 != 1);
+                printf("\nEnter the number of numbers: ");
+                n=getValidInteger();
                 result = subtract(n);
+                if(flag) printf("Result: %lf\n", result);
                 if(result == 0) flag = 0;
                 break;
+                
             case 3:
-                printf("Enter the number of numbers: ");
-                do{
-                    check1 = scanf(" %d", &n);
-                    if(check1 != 1) {
-                        printf("Error: Invalid input. Please enter a valid number:");
-                        fflush(stdin);
-                    }
-                }while(check1 != 1);
+                printf("\nEnter the number of numbers: ");
+                n=getValidInteger();
                 result = multiply(n);
+                if(flag) printf("Result: %lf\n", result);
                 if(result == 0) flag = 0;
                 break;
+                
             case 4:
                 result = divide();
+                printf("\nQuotient: %lf\n",result);
                 if(result == 0) flag = 0;
                 break;
+                
             case 5:
                 result = modulus();
+                printf("\nRemainder: %lf\n",result);
                 if(result == 0) flag = 0;
                 break;
             case 6:
-            printf("Exiting The Program");
-            exit(0);
+                
+                printf("Exiting The Program");
+                exit(0);
+                
             default:
                 printf("Error: Invalid operation\n");
                 return 0;
         }
+        
         if(flag) printf("Result: %lf\n", result);
         if(flag) 
         {
