2030b0a9e29a1a08933e313c19b438666caa2028
Shahin M Hashim <98640366+shahin-m-hashim@users.noreply.github.com>
2023-01-20 10:46:06 +0530

    added new fns
    added logarithmic,gcd_lcm,prime,and bitwise operations

--- sm_calculator.c
+++ sm_calculator.c
@@ -317,6 +317,124 @@ double TrigFunction()
     return 0;
 }
 
+//LOGORTHMIC FUNCTION
+
+void logarithmicFunction()
+{
+    double input;
+    printf("\nEnter a number: ");
+    input=getValidInput();
+
+    double natural_log = log(input);
+    double base10_log = log10(input);
+
+    printf("The natural log of %lf is %lf\n", input, natural_log);
+    printf("The base 10 log of %lf is %lf\n", input, base10_log);
+}
+
+// PRIME OR NOT
+
+int isPrime() 
+{
+    int num, i, flag = 0;
+    printf("Enter a number: ");
+    num=getValidInput();
+    for(i = 2; i <= num/2; ++i) 
+    {
+        if(num % i == 0) 
+        {
+            flag = 1;
+            break;
+        }
+    }
+    if (flag == 1)
+        printf("%d is not a prime number.\n", num);
+    else
+        printf("%d is a prime number.\n", num);
+    return 0;
+}
+
+// GCD & LCM
+
+void calculate_GCD_LCM() 
+{
+    int num1, num2, gcd, lcm;
+
+    // Get user input for first number
+    printf("Enter first number: ");
+    num1=getValidInput();
+
+    // Get user input for second number
+    printf("Enter second number: ");
+    num2=getValidInput();
+
+    // Calculate GCD
+    for (int i = 1; i <= num1 && i <= num2; i++) 
+    {
+        if (num1 % i == 0 && num2 % i == 0) 
+        {
+            gcd = i;
+        }
+    }
+
+    // Calculate LCM
+    lcm = (num1 * num2) / gcd;
+
+    // Print results
+    printf("GCD of %d and %d is %d\n", num1, num2, gcd);
+    printf("LCM of %d and %d is %d\n", num1, num2, lcm);
+}
+
+//BITWISE OPERATIONS
+
+void BitwiseOperations() 
+{
+    char ch;
+    int choice, num1, num2;
+    printf("\n1. AND\n2. OR\n3. NOT\n4. XOR\n");
+    do
+    {
+        int flag=1;
+        printf("Select a Bitwise Operation:");
+        choice=getValidInput();
+        printf("Enter first number: ");
+        num1=getValidInput();
+        printf("Enter second number: ");
+        num2=getValidInput();
+        switch(choice) 
+        {
+            case 1:
+                printf("Result: %d\n", num1 & num2);
+                break;
+            case 2:
+                printf("Result: %d\n", num1 | num2);
+                break;
+            case 3:
+                printf("Result: %d\n", ~num1);
+                break;
+            case 4:
+                printf("Result: %d\n", num1 ^ num2);
+                break;
+            default:
+                printf("Invalid option\n");
+        }if(flag) 
+        {
+            printf("\nDo you want to continue The Trignometric Functions(Y/N) ?\nChoice:");
+            scanf(" %c", &ch);
+            while(ch != 'Y' && ch != 'y' && ch != 'N' && ch != 'n') 
+            {
+                printf("Error: Invalid input. Please enter either 'Y' or 'N'\nChoice:");
+                fflush(stdin);
+                scanf(" %c", &ch);
+            }
+        }
+        else
+        {
+            ch = 'N';
+        }
+    }while(ch== 'Y' || ch == 'y');
+}
+
 // MAIN APPLICATION
 
 int main() 
@@ -324,8 +442,7 @@ int main()
     char choice;
     double result;
     int operation,n,flag;
-    
-    printf("\nAvailable Operations\n1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n5.Modulus\n6.Square Root\n7.Cubic Root\n8.Exponentiation(Power Of A Number)\n9.Factorial Of A Number\n10.Trigonometric functions\n11.Exit\n");
+    printf("\nAvailable Operations\n1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n5.Modulus\n6.Square Root\n7.Cubic Root\n8.Exponentiation(Power Of A Number)\n9.Factorial Of A Number\n10.Trigonometric functions\n11.Logarithmic functions (natural log, base 10 log)\n12.Prime number check\n13.GCD (Greatest common divisor) and LCM (Least common multiple)\n14.Bitwise operations (AND, OR, NOT, XOR)\n15.Exit\n");
     do
     {
         flag=1;
@@ -333,8 +450,6 @@ int main()
         operation=getValidInput();
 
         switch(operation) 
-        {
-switch(operation) 
         {
             case 1:         
                 printf("\nEnter the number of numbers: ");
@@ -349,7 +464,6 @@ switch(operation)
                 if(flag) printf("Result: %lf\n", result);
                 if(result == 0) flag = 0;
                 break;
-        
             case 3:
                 printf("\nEnter the number of numbers: ");
                 n=getValidInput();
@@ -357,59 +471,66 @@ switch(operation)
                 if(flag) printf("Result: %lf\n", result);
                 if(result == 0) flag = 0;
                 break;
-        
             case 4:
                 result = divide();
                 printf("\nQuotient: %lf\n",result);
                 if(result == 0) flag = 0;
                 break;
-        
             case 5:
                 result = modulus();
                 printf("\nRemainder: %lf\n",result);
                 if(result == 0) flag = 0;
                 break;
-        
             case 6:
                 result = findSquareRoot();
                 printf("\nSquare Root: %lf\n",result);
                 if(result == 0) flag = 0;
                 break;
-        
             case 7:
                 result = findCubicRoot();
                 printf("\nCubic Root: %lf\n",result);
                 if(result == 0) flag = 0;
                 break;
-        
             case 8:
                 result = power();
                 printf("\nResult: %lf\n",result);
                 if(result == 0) flag = 0;
                 break;
-        
             case 9:
                 result=factorial();
                 printf("\nFactorial: %lf\n",result);
                 if(result == 0) flag = 0;
                 break;
-        
             case 10:
                 TrigFunction();
                 if(result == 0) flag = 0;
                 break;
-                
-            case 11:                
-                printf("Exiting The Program");
+            case 11:
+                logarithmicFunction();
+                if(result == 0) flag = 0;
+                break;
+            case 12:
+                isPrime();
+                if(result == 0) flag = 0;
+                break;          
+            case 13:
+                calculate_GCD_LCM();
+                if(result == 0) flag = 0;
+                break;         
+            case 14:
+                BitwiseOperations();
+                if(result == 0) flag = 0;
+                break;
+            case 15:
+                printf("\nExiting The Program");
                 exit(0);
-                
             default:
-                printf("Error: Invalid operation\n");
+                printf("\nError: Invalid operation\n");               
                 return 0;
         }
         if(flag) 
         {
-            printf("Do you want to continue (Y/N) ?\nChoice:");
+            printf("\nDo you want to continue with the basic Operations(Y/N) ?\nChoice:");
             scanf(" %c", &choice);
             while(choice != 'Y' && choice != 'y' && choice != 'N' && choice != 'n') 
             {
